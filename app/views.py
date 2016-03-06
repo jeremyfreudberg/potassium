02f324688799737e08646f64f19c69e608f6fd36
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2016-03-06 09:28:01 -0500

    Handle incomplete form input

--- app/views.py
+++ app/views.py
@@ -1,21 +1,25 @@
-from flask import render_template, request
+from flask import render_template, request, flash
 from app import app
 from .forms import Nutrition
 from .api import process
 from .format_results import nice
+
 @app.route('/', methods=['GET', 'POST'])
 def info():
     form = Nutrition()
     if request.method == 'POST':
-        food = request.form.get("food")
-        element = request.form.get("element")
-        amount = request.form.get("amount")
-        unit = request.form.get("unit")
-        from_api = process(element, food, amount, unit)
-        query_raw = from_api[0]
-        mg = from_api[1]
-        query = nice(query_raw, element)
-        return render_template('results.html', query=query, mg=mg)
+        if form.validate_on_submit():
+            food = request.form.get("food")
+            element = request.form.get("element")
+            amount = request.form.get("amount")
+            unit = request.form.get("unit")
+            from_api = process(element, food, amount, unit)
+            query_raw = from_api[0]
+            mg = from_api[1]
+            query = nice(query_raw, element)
+            return render_template('results.html', query=query, mg=mg)
+        else:
+            return render_template('form.html',form=form)
     elif request.method == 'GET':
         return render_template('form.html',form=form)
     

a2d249556061ef800918b1106fe4341c0b1a10f3
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2016-03-12 14:16:03 -0500

    Support for Google query

--- app/views.py
+++ app/views.py
@@ -12,16 +12,21 @@ def info():
             food = request.form.get("food")
             amount = request.form.get("amount")
             unit = request.form.get("unit")
-            from_api = process(food, amount, unit)
-            potassium = from_api[0]
-            sodium = from_api[1]
-            potassium_raw = potassium[0]
-            potassium_mg = potassium[1][:-13]
-            sodium_raw = sodium[0]
-            sodium_mg = sodium[1][:-13]
-            potassium_nice = nice(potassium_raw, 'Potassium')
-            sodium_nice = nice(sodium_raw, 'Sodium')
-            return render_template('results.html', k=potassium_nice, k_mg=potassium_mg, na=sodium_nice, na_mg=sodium_mg)
+            try:
+                from_api = process(food, amount, unit)
+                potassium = from_api[0]
+                sodium = from_api[1]
+                potassium_raw = potassium[0]
+                potassium_mg = potassium[1][:-13]
+                sodium_raw = sodium[0]
+                sodium_mg = sodium[1][:-13]
+                potassium_nice = nice(potassium_raw, 'Potassium')
+                sodium_nice = nice(sodium_raw, 'Sodium')
+                return render_template('results.html', k=potassium_nice, k_mg=potassium_mg, na=sodium_nice, na_mg=sodium_mg)
+            except IndexError:
+                query = '{} {} of {}'.format(amount,unit,food)
+                return render_template('wolfram_fail.html',\
+                                       query=query)
         else:
             return render_template('form.html',form=form)
     elif request.method == 'GET':
@@ -30,3 +35,7 @@ def info():
 @app.errorhandler(500)
 def internal(e):
     return render_template('error.html'), 500
+
+@app.route('/test')
+def test():
+    return render_template('wolfram_fail.html')
